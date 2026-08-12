ad4e43c3feb2c5c63cb3738e6f6118f8e08b964e
Mrjello0 <101129330+Mrjello0@users.noreply.github.com>
2026-08-12 06:31:48 -0500

    Add files via upload
    vibecoded

--- rocksmith-launcher.sh
+++ rocksmith-launcher.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-cd /home/me/.steam/steam/steamapps/common/Rocksmith2014/
-
-export PIPEWIRE_LATENCY="128/48000"
-export LD_PRELOAD=/usr/lib/libjack.so
-
-SteamAppId=221680 \
-STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/me/.local/share/Steam \
-STEAM_COMPAT_DATA_PATH=/home/me/.local/share/Steam/steamapps/compatdata/221680 \
-"/home/me/.local/share/Steam/steamapps/common/Proton 10.0/proton" \
-waitforexitandrun \
-"./Rocksmith2014.exe" -uplay_steam_mode
+cd /steamapps/common/Rocksmith2014/
+PIPEWIRE_LATENCY="256/48000" 
